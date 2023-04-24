f22789234409af6307059a003a7230eec9cc89b3
Santiago Zuluaga <szuluaga@truora.com>
2023-04-24 10:07:25 -0500

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <a
+      href="https://identity.truorastaging.com/preview/IPF77c3e61eaf6d12f043b0e1b192b176ed"
+      style="
+        text-decoration: none; background: #01022E;
+        color: #ffffff; border-radius: 6px;
+        display: inline-block; padding: 8px 32px;
+      "
+    >
+      Validation with Truora
+    </a>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Install
+    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
